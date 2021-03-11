31d5e0eb827957fb2119d6d955e32ccef4d66423
jenkins <jenkins@localhost.localdomain>
2021-03-11 15:00:51 +0530

    testing

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,11 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-<<<<<<< HEAD
-    private final String message = "Hello World from git and jenkins!";
-=======
-    private final String message = "Hello World from git!";
->>>>>>> d5131d3b6581f9ee96d52d1addd44d9852bcf007
+    private final String message = "Hello World from";
 
     public App() {}
 
